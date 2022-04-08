6c0ecf7b768f0af58f1350cf7349eba7dca7a14a
Ibnul Ashir <ibnulashir42@gmail.com>
2022-04-08 15:06:59 +0600

    register, login, logout

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
-import { useState } from 'react'
-import { FaSignInAlt } from 'react-icons/fa'
+import { useState, useEffect } from 'react'
+import { FaSignInAlt } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { login, reset } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 function Login() {
 
@@ -10,6 +15,22 @@ function Login() {
 
     const { email, password } = formData;
 
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const { user, isLoading, isSuccess, isError, message } = useSelector(state => state.auth)
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+        if (isSuccess || user) {
+            navigate('/');
+        }
+
+        dispatch(reset());
+    }, [user, isLoading, isSuccess, isError, message, navigate, dispatch]);
+
     const onChange = (e) => {
         setFormData((prevState) => ({
             ...formData,
@@ -19,6 +40,15 @@ function Login() {
 
     const onSubmit = (e) => {
         e.preventDefault();
+        const userData = {
+            email,
+            password
+        }
+        dispatch(login(userData));
+    }
+
+    if (isLoading) {
+        return <Spinner />
     }
 
     return (
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,10 @@
-import React, { useState } from 'react';
-import { FaUser } from 'react-icons/fa'
+import React, { useState, useEffect } from 'react';
+import { FaUser } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { register, reset } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -11,14 +16,43 @@ function Register() {
 
     const { name, email, password, password2 } = formData;
 
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const { user, isLoading, isSuccess, isError, message } = useSelector(state => state.auth)
+
     const onChange = (e) => {
         setFormData((prevState) => ({
             ...formData,
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         }))
     }
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+        if (isSuccess || user) {
+            navigate('/');
+        }
+
+        dispatch(reset());
+    }, [user, isLoading, isSuccess, isError, message, navigate, dispatch]);
     const onSubmit = (e) => {
         e.preventDefault();
+        if (password !== password2) {
+            toast.error('Password do not match')
+        } else {
+            const userData = {
+                name,
+                email,
+                password
+            }
+            dispatch(register(userData));
+        }
+    }
+    if (isLoading) {
+        return <Spinner />
     }
     return (
         <>
@@ -65,7 +99,7 @@ function Register() {
                     </div>
                     <div className="form-group">
                         <input
-                            type="password2"
+                            type="password"
                             className="form-control"
                             id='password2'
                             name='password2'
